298b7af024e02c3a36ce3205685d20194963f03d
Md Rakibul Islam <bdsky25@gmail.com>
2024-01-16 17:15:16 +0600

    Implement Search Bar on the Home page design with responsive

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import SearchBar from '../SearchBar/SearchBar';
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <SearchBar />
         </>
     );
 };
---
+++ src/pages/Home/SearchBar/SearchBar.jsx
@@ -0,0 +1,28 @@
+import { CiSearch } from 'react-icons/ci';
+
+const SearchBar = () => {
+    return (
+        <div className='container px-5 mx-auto py-10'>
+            <form className='lg:w-3/4 mx-auto'>
+                <div className='grid lg:grid-cols-2 items-center gap-5 p-[22px] bg-pink-600 rounded-[51px] shadow'>
+                    <div className='flex items-center justify-center lg:justify-start gap-5'>
+                        <button type='submit' className='bg-[#FAE527] rounded-full p-3'>
+                            <CiSearch className='w-[42px] h-[42px]' />
+                        </button>
+                        <h4 className="text-white text-[32px] font-semibold font-['Nunito'] leading-normal">
+                            Find your product
+                        </h4>
+                    </div>
+                    <input
+                        className="bg-white rounded-[29px] text-stone-600 text-2xl font-semibold font-['Nunito'] leading-normal outline-none py-2 h-[58px] w-full px-4"
+                        type='text'
+                        placeholder='Search'
+                        id='search'
+                    />
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SearchBar;
